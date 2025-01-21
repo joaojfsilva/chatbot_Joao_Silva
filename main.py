b4151d2aa7e94536dada773ebc1696736e624488
joaojfsilva <joaofs91@outlook.com>
2025-01-21 14:06:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 print("Welcome!")
 print(" ")
-birds = input("How many Budgies do you have: ")
-print(f"You have: {birds} Budgies")
+birds = input("How many birds do you have?  ")
+print(f"You have: {birds} Birds")
+
+year = input("What year are your birds from? ")
+print(f"Your birds were born in {year}")
+
+print(f"You have {birds} birds of {year}")
