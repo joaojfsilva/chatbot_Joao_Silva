c82a4840c1cdfdc7812f412f9eac2d2d12a22603
joaojfsilva <joaofs91@outlook.com>
2025-01-21 14:12:45 +0000

    Versao Final
    Adicionei o restante codigo e espacos entre respostas

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 print("Welcome!")
 print(" ")
+
 birds = input("How many birds do you have?  ")
+print(" ")
+
 print(f"You have: {birds} Birds")
+print(" ")
 
 year = input("What year are your birds from? ")
+print(" ")
+
 print(f"Your birds were born in {year}")
+print(" ")
 
 print(f"You have {birds} birds of {year}")
+print(" ")
